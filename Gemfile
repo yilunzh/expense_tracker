42ecd91bbd71feb7a5831b4fb83bd08e7004698b
Yilun Zhang <yilunzh@Yiluns-MacBook-Pro.local>
2015-04-12 11:22:25 -0700

    added user authentication

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ gem 'bootstrap-sass', '~> 3.2.0'
 
 gem 'autoprefixer-rails'
 
+gem 'devise'
+
 group :development, :test do
   gem 'rspec-rails'
   gem 'cucumber'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   resources :transactions
   match '/summary', to: 'transactions#summary', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
